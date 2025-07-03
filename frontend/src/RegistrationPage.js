26db6eedd1db891ea551d5145d1a5a247bb43746
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2025-07-03 23:19:24 +0530

    Update RegistrationPage.js

--- frontend/src/RegistrationPage.js
+++ frontend/src/RegistrationPage.js
@@ -20,7 +20,7 @@ const handleRegistrationChange = (e) => {
 const handleRegistrationSubmit = async(e) => {
    e.preventDefault();
    try{
-    const response = await axios.post('http://localhost:8000/register',registrationData)
+    const response = await axios.post('https://mdbtt.onrender.com/register',registrationData)
     console.log(response.data);
    }
    catch(error){
